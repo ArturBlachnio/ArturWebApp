6dfe442bce4fe8ebf5e15d4f091f06233bef0708
Artur Blachnio <blachnio.artur@gmail.com>
2021-01-30 21:07:55 +0100

    AWA#5. Now due_date is visible when task is expanded to buttons.

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from awa import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.2.17', port=5000)
+    app.run(debug=True, host='192.168.2.18', port=5000)
     # app.run(debug=True)
